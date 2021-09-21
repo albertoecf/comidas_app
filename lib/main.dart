316298c3f41b8b739b8634f577381bf94d37c263
aecf <81099969+albertoecf@users.noreply.github.com>
2021-09-21 20:12:26 -0300

    add meal info. plus navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@
 //import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import './category_meals_screen.dart';
-import './categories_screen.dart';
+import './models/meal.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,31 +13,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-        accentColor: Colors.blueGrey,
-        canvasColor: Colors.grey,
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-              body1: TextStyle(
-                color: Colors.black87,
+        title: 'DeliMeals',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          accentColor: Colors.blueGrey,
+          canvasColor: Colors.grey,
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(
+                  color: Colors.black87,
+                ),
+                body2: TextStyle(
+                  color: Colors.blueGrey.shade100,
+                ),
+                title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotCondensed',
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              body2: TextStyle(
-                color: Colors.blueGrey.shade100,
-              ),
-              title: TextStyle(
-                fontSize: 20,
-                fontFamily: 'RobotCondensed',
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-      ),
-      //home: CategoriesScreen(), siempre puedo llamar a home con "/"
-      routes: { '/': (ctx) => CategoriesScreen(),
-      CategoryMealsScreen.routeName : (ctx) => CategoryMealsScreen()
-      //  '/category-meals':(ctx) =>CategoryMealsScreen() - podemos armar una propiedad 
-      }
-    );
+        ),
+        //home: CategoriesScreen(), siempre puedo llamar a home con "/"
+        routes: {
+          '/': (ctx) => CategoriesScreen(),
+          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+          MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+          //  '/category-meals':(ctx) =>CategoryMealsScreen() - podemos armar una propiedad
+        });
   }
 }
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './category_meals_screen.dart';
+import '../screens/category_meals_screen.dart';
 //import 'package:flutter_complete_guide/models/category.dart';
 
 class CategoryItem extends StatelessWidget {

644ecf419fed785a1cb3a50dfc2f853322e24589
aecf <81099969+albertoecf@users.noreply.github.com>
2021-09-22 08:50:59 -0300

    improve Meal detail Page

--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'DeliMeals',
-        theme: ThemeData(
-          primarySwatch: Colors.orange,
-          accentColor: Colors.blueGrey,
-          canvasColor: Colors.grey,
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-                body1: TextStyle(
-                  color: Colors.black87,
-                ),
-                body2: TextStyle(
-                  color: Colors.blueGrey.shade100,
-                ),
-                title: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'RobotCondensed',
-                  fontWeight: FontWeight.bold,
-                ),
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        accentColor: Colors.blueGrey,
+        canvasColor: Colors.grey,
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Colors.black87,
               ),
-        ),
-        //home: CategoriesScreen(), siempre puedo llamar a home con "/"
-        routes: {
-          '/': (ctx) => CategoriesScreen(),
-          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-          MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-          //  '/category-meals':(ctx) =>CategoryMealsScreen() - podemos armar una propiedad
-        });
+              body2: TextStyle(
+                color: Colors.blueGrey.shade100,
+              ),
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+      //home: CategoriesScreen(), siempre puedo llamar a home con "/"
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        //  '/category-meals':(ctx) =>CategoryMealsScreen() - podemos armar una propiedad
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
+      // similar to 404 in web - un salvavida para cuando no anda
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
+    );
   }
 }
